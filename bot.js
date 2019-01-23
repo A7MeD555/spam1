ec3a5e427d7095881c650c5a7da6a71c72dab630
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2019-01-23 11:07:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var prefix = "1";
 client.on('ready', () => {
     console.log('I am ready!');
+});
+
+
+ client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -say
+
+  if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
 });
 
 
 
 
 
-
-
-// CODES HERE
+client.on("ready", () => {
+let channel =     client.channels.get("499686836751302683")
+setInterval(function() {
+channel.send(`ادينا رمضان احية احية اديلوة احية احية احية احية هخhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh:joy: :joy: hhhhhhhhhhhhgggggggggggggg`);
+}, 25)
+})
 
 
 
